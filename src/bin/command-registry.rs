e8ce605eeef51c45ecca82cf40e3cced35feb382
ssnover95 <sws7379@rit.edu>
2020-04-02 20:10:23 -0400

    Make receivers mut, add more explanation to README

--- src/bin/command-registry.rs
+++ src/bin/command-registry.rs
@@ -1,16 +1,19 @@
 use std::collections::{HashMap, VecDeque};
+use std::sync::{Arc, Mutex};
 
 #[derive(PartialEq, Eq, Hash)]
 enum CommandId {
     SimpleDataRequest,
     ComplexDataRequest,
     UnimplementedRequest,
+    MoreSimpleRequest,
 }
 
 enum CommandArgs {
     SimpleDataRequestArgs { serial_number: u32, object_type: u8 },
     ComplexDataRequestArgs { payload: Vec<u8> },
     UnimplementedRequestArgs,
+    MoreSimpleRequestArgs,
 }
 
 struct Command {
@@ -19,7 +22,7 @@ struct Command {
 }
 
 trait CommandReceiver {
-    fn handle_command(&self, event: CommandArgs) -> ();
+    fn handle_command(&mut self, event: CommandArgs) -> ();
 }
 
 pub struct SimpleReceiver {
@@ -37,15 +40,23 @@ impl SimpleReceiver {
             self.data_link
         );
     }
+
+    fn handle_more_simple(&mut self) -> () {
+        println!("simple-receiver: That was easy!");
+    }
 }
 
+unsafe impl Send for SimpleReceiver {}
+unsafe impl Sync for SimpleReceiver {}
+
 impl CommandReceiver for SimpleReceiver {
-    fn handle_command(&self, event: CommandArgs) {
+    fn handle_command(&mut self, event: CommandArgs) {
         match event {
             CommandArgs::SimpleDataRequestArgs {
                 serial_number,
                 object_type,
             } => self.handle_simple_data(serial_number, object_type),
+            CommandArgs::MoreSimpleRequestArgs => self.handle_more_simple(),
             _ => eprintln!("simple-receiver: Received unknown event, dropping it."),
         }
     }
@@ -56,7 +67,7 @@ pub struct ComplexReceiver {
 }
 
 impl ComplexReceiver {
-    fn handle_complex_data(&self, payload: Vec<u8>) -> () {
+    fn handle_complex_data(&mut self, payload: Vec<u8>) -> () {
         println!(
             "complex-receiver: Received payload with length {} bytes, logging to logfile: {}",
             payload.len(),
@@ -65,8 +76,11 @@ impl ComplexReceiver {
     }
 }
 
+unsafe impl Send for ComplexReceiver {}
+unsafe impl Sync for ComplexReceiver {}
+
 impl CommandReceiver for ComplexReceiver {
-    fn handle_command(&self, event: CommandArgs) {
+    fn handle_command(&mut self, event: CommandArgs) {
         match event {
             CommandArgs::ComplexDataRequestArgs { payload } => self.handle_complex_data(payload),
             _ => eprintln!("complex-receiver: Received unknown event, dropping it."),
@@ -108,28 +122,38 @@ fn main() {
         cmd: CommandId::UnimplementedRequest,
         args: CommandArgs::UnimplementedRequestArgs,
     });
+    command_queue.push_back(Command {
+        cmd: CommandId::MoreSimpleRequest,
+        args: CommandArgs::MoreSimpleRequestArgs,
+    });
+
+    let simple_receiver: Arc<Mutex<dyn CommandReceiver>> = Arc::new( Mutex::new(SimpleReceiver {data_link: "UART0".to_string() }));
+    let complex_receiver: Arc<Mutex<dyn CommandReceiver>> = Arc::new(Mutex::new(ComplexReceiver {logger_name: "/sys/log".to_string()}));
 
     // Register the receivers for each command they handle
-    let mut command_receiver_registry: HashMap<CommandId, Box<dyn CommandReceiver>> =
+    let mut command_receiver_registry =
         HashMap::new();
     command_receiver_registry.insert(
         CommandId::SimpleDataRequest,
-        Box::new(SimpleReceiver {
-            data_link: "UART0".to_string(),
-        }),
-    );
+        Arc::clone(&simple_receiver));
     command_receiver_registry.insert(
         CommandId::ComplexDataRequest,
-        Box::new(ComplexReceiver {
-            logger_name: "/sys/log".to_string(),
-        }),
-    );
+        Arc::clone(&complex_receiver));
+    command_receiver_registry.insert(
+        CommandId::MoreSimpleRequest,
+        Arc::clone(&simple_receiver));
 
     // Process the commands
     while command_queue.len() > 0 {
         let next = command_queue.pop_front().unwrap();
         match command_receiver_registry.get(&next.cmd) {
-            Some(receiver) => receiver.handle_command(next.args),
+            Some(receiver) => {
+                if let Ok(ref mut receiver) = receiver.lock() {
+                    receiver.handle_command(next.args);
+                } else {
+                    eprintln!("command-server: Unable to acquire access to receiver.")
+                }
+            },
             None => eprintln!("command-server: Received command with no receiver registered, dropping it on the floor."),
         }
     }
